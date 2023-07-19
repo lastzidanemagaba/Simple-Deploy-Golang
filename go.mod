ade5151ff515cb6e65eea4e6748ebbbedc161d34
lastzidanemagaba <lastzidanemagaba@gmail.com>
2023-07-19 23:40:08 +0700

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lastzidanemagaba/Simple-Deploy-Golang
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	handler := http.NewServeMux()
+	handler.HandleFunc("/api/hello", SayHello)
+	http.ListenAndServe(":8989", handler)
+	//we tell our api to listen to all request to port 8080.
+}
+func SayHello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, `Hello world`)
+}
